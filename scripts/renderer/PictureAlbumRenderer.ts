612d421ace656048570e66f3e70277d978d39374
Simon Urli <simon.urli@gmail.com>
2015-04-08 16:29:10 +0200

    CLIENT-15 Renderer pictureAlbum is now working ! \o/

--- scripts/renderer/PictureAlbumRenderer.ts
+++ scripts/renderer/PictureAlbumRenderer.ts
@@ -5,30 +5,51 @@
 
 /// <reference path="../../t6s-core/core/scripts/infotype/PictureAlbum.ts" />
 /// <reference path="../../t6s-core/core/scripts/infotype/Picture.ts" />
+/// <reference path="../../t6s-core/core/scripts/infotype/PictureURL.ts" />
 /// <reference path="../policy/RenderPolicy.ts" />
 /// <reference path="./Renderer.ts" />
+
+declare var $: any; // Use of JQuery
 
 class PictureAlbumRenderer implements Renderer<Picture> {
     transformForBehaviour(listInfos : Array<PictureAlbum>, renderPolicy : RenderPolicy<PictureAlbum>) : Array<Picture> {
         console.log("List d'informations : ");
         console.log(listInfos);
-        var listPictureAlbums = renderPolicy.process(listInfos);
+        var listPictureAlbums : Array<PictureAlbum> = renderPolicy.process(listInfos);
         console.log("List processed : ");
         console.log(listPictureAlbums);
 
         var result = new Array<Picture>();
-        listPictureAlbums.forEach(function(pictureAlbum : PictureAlbum) {
-            pictureAlbum.getPictures().forEach(function (picture : Picture) {
+
+        listPictureAlbums.forEach(function(pictureAlbumJSON : any) {
+            var pictureAlbum : PictureAlbum = PictureAlbum.fromJSONObject(pictureAlbumJSON);
+
+            console.log("Picture album :");
+            console.log(pictureAlbum);
+
+            var pictures : Array<Picture> = pictureAlbum.getPictures();
+
+            pictures.forEach(function (pictureJSON : Picture) {
+                var picture : Picture = Picture.fromJSONObject(pictureJSON);
+                console.log("Valeur de reuslt :");
+                console.log(result);
                 result.push(picture);
-            })
+            });
         });
 
+        console.log("Result final");
+        console.log(result);
         return result;
     }
 
     render(info : Picture, domElem : any) {
+        console.log("Info reçue : ");
+        console.log(info);
         var pictureHTML = $("<img>");
-        pictureHTML.src(info.getMedium().getURL());
+        var pictureUrl : any = info.getMedium();
+        console.log("URL : ");
+        console.log(pictureUrl);
+        pictureHTML.attr('src',pictureUrl._url);
 
         $(domElem).empty();
         $(domElem).append(pictureHTML);
