d78b5daceecb6033b872b93beac8911a69c1e6a4
Christian Brel <ch.brel@gmail.com>
2015-06-01 00:22:01 +0200

    refs CLIENT-6 : Updated less files to manage themes in Client.

--- scripts/core/Utils.ts
+++ scripts/core/Utils.ts
@@ -30,4 +30,56 @@ class Utils {
             failCB(new Error("Server response is not well formatted."));
         }
     }
+
+	/**
+	 * Methods to help conversion of hexadecimal color into RGB color.
+	 */
+
+	/**
+	 * @method cutHex
+	 * @static
+	 * @param {string} h - The Hexadecimal color.
+	 */
+	static cutHex(h : string) {
+		return (h.charAt(0)=="#") ? h.substring(1,7):h
+	}
+
+	/**
+	 * @method hexToR
+	 * @static
+	 * @param {string} h - The Hexadecimal color.
+	 */
+	static hexToR(h : string) {
+		return parseInt((Utils.cutHex(h)).substring(0,2),16)
+	}
+
+	/**
+	 * @method hexToG
+	 * @static
+	 * @param {string} h - The Hexadecimal color.
+	 */
+	static hexToG(h : string) {
+		return parseInt((Utils.cutHex(h)).substring(2,4),16)
+	}
+
+	/**
+	 * @method hexToB
+	 * @static
+	 * @param {string} h - The Hexadecimal color.
+	 */
+	static hexToB(h : string) {
+		return parseInt((Utils.cutHex(h)).substring(4,6),16)
+	}
+
+	/**
+	 * Method to return RGBA color from hexadecimal color and opacity
+	 *
+	 * @method toRGBA
+	 * @static
+	 * @param {string} h - The Hexadecimal color.
+	 * @param {string} opacity - The color's opacity.
+	 */
+	static toRGBA(h : string, opacity : string) {
+		return "rgba(" + Utils.hexToR(h) + "," + Utils.hexToG(h) + "," + Utils.hexToB(h) + "," + opacity + ")";
+	}
 }
