36d1940d43d0bd4b3851a735c6e2684445558c45
Christian Brel <ch.brel@gmail.com>
2015-06-25 23:50:07 +0200

    refs SERVICES-49 : Added a first renderer for CounterList.

---
+++ scripts/renderer/TweetCounterRenderer.ts
@@ -0,0 +1,59 @@
+/**
+ * @author Christian Brel <christian@the6thscreen.fr, ch.brel@gmail.com>
+ * @author Simon Urli <simon@the6thscreen.fr, simon.urli@gmail.com>
+ */
+
+/// <reference path="../../t6s-core/core/scripts/infotype/CounterList.ts" />
+/// <reference path="../../t6s-core/core/scripts/infotype/Counter.ts" />
+/// <reference path="./Renderer.ts" />
+
+declare var $: any; // Use of JQuery
+declare var moment: any; // Use of MomentJS
+
+class TweetCounterRenderer implements Renderer<Counter> {
+	/**
+	 * Transform the Info list to another Info list.
+	 *
+	 * @method transformInfo<ProcessInfo extends Info>
+	 * @param {ProcessInfo} info - The Info to transform.
+	 * @return {Array<RenderInfo>} listTransformedInfos - The Info list after transformation.
+	 */
+	transformInfo(info : CounterList) : Array<Counter> {
+		var counterLists : Array<CounterList> = new Array<CounterList>();
+		try {
+			var newInfo = CounterList.fromJSONObject(info);
+			counterLists.push(newInfo);
+		} catch(e) {
+			Logger.error(e.message);
+		}
+
+		var counters : Array<Counter> = new Array<Counter>();
+
+		for(var iCL in counterLists) {
+			var cl : CounterList = counterLists[iCL];
+			var clCounters : Array<Counter> = cl.getCounters();
+			for(var iC in clCounters) {
+				var c : Counter = clCounters[iC];
+				counters.push(c);
+			}
+		}
+
+		return counters;
+	}
+
+	/**
+	 * Render the Info in specified DOM Element.
+	 *
+	 * @method render
+	 * @param {RenderInfo} info - The Info to render.
+	 * @param {DOM Element} domElem - The DOM Element where render the info.
+	 */
+	render(info : Counter, domElem : any) {
+
+		var counterHTMLWrapper = $("<div>");
+		counterHTMLWrapper.addClass("TweetCounterRenderer_wrapper");
+		counterHTMLWrapper.html(info.getValue());
+
+		$(domElem).append(counterHTMLWrapper);
+	}
+}
