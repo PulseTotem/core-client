2a42aeb98126e4abd19f82673745936c36c4bdbf
Simon Urli <simon.urli@gmail.com>
2015-07-09 18:10:42 +0200

    SERVICES-53 Create renderer for photobox

---
+++ scripts/renderer/PhotoboxRenderer.ts
@@ -0,0 +1,102 @@
+/**
+ * @author Simon Urli <simon@the6thscreen.fr, simon.urli@gmail.com>
+ */
+
+/// <reference path="../../t6s-core/core/scripts/infotype/CmdList.ts" />
+/// <reference path="../../t6s-core/core/scripts/infotype/EventList.ts" />
+/// <reference path="./Renderer.ts" />
+
+declare var $: any; // Use of JQuery
+declare var Webcam: any; // use of WebcamJS
+
+class PhotoboxRenderer implements Renderer<Cmd> {
+	/**
+	 * Transform the Info list to another Info list.
+	 *
+	 * @method transformInfo<ProcessInfo extends Info>
+	 * @param {ProcessInfo} info - The Info to transform.
+	 * @return {Array<RenderInfo>} listTransformedInfos - The Info list after transformation.
+	 */
+	transformInfo(info : CmdList) : Array<Cmd> {
+		var newListInfos : Array<CmdList> = new Array<CmdList>();
+		try {
+			var newInfo = CmdList.fromJSONObject(info);
+			newListInfos.push(newInfo);
+		} catch(e) {
+			Logger.error(e.message);
+		}
+
+		var result = new Array<Cmd>();
+
+		newListInfos.forEach(function(cmdList : CmdList) {
+			var cmds : Array<Cmd> = cmdList.getCmds();
+
+			cmds.forEach(function (cmd : Cmd) {
+				result.push(cmd);
+			});
+		});
+
+		return result;
+	}
+
+	/**
+	 * Render the Info in specified DOM Element.
+	 *
+	 * @method render
+	 * @param {RenderInfo} info - The Info to render.
+	 * @param {DOM Element} domElem - The DOM Element where render the info.
+	 * @param {Function} endCallback - Callback function called at the end of render method.
+	 */
+	render(info : Cmd, domElem : any, endCallback : Function) {
+
+		if (info.getCmd() == "startSession") {
+			this.startSession(domElem);
+		}
+
+		endCallback();
+	}
+
+	private startSession(domElem : any) {
+		var html = $('div');
+		var title = $('h3');
+		title.content("Cliquez sur le bouton pour démarrer le timer quand vous êtes prêts à prendre la photo !");
+		html.append(title);
+
+		var divCam = $('div');
+		divCam.id = "webCamview";
+		divCam.style.width = "100%";
+		divCam.style.height = "100%";
+
+		html.append(divCam);
+		domElem.append(divCam);
+
+		Webcam.attach(divCam);
+	}
+
+	/**
+	 * Update rendering Info in specified DOM Element.
+	 *
+	 * @method updateRender
+	 * @param {RenderInfo} info - The Info to render.
+	 * @param {DOM Element} domElem - The DOM Element where render the info.
+	 * @param {Function} endCallback - Callback function called at the end of updateRender method.
+	 */
+	updateRender(info : CityEvent, domElem : any, endCallback : Function) {
+		// nothing to do
+		endCallback();
+	}
+
+	/**
+	 * Animate rendering Info in specified DOM Element.
+	 *
+	 * @method animate
+	 * @param {RenderInfo} info - The Info to animate.
+	 * @param {DOM Element} domElem - The DOM Element where animate the info.
+	 * @param {Function} endCallback - Callback function called at the end of animation.
+	 */
+	animate(info : CityEvent, domElem : any, endCallback : Function) {
+		//Nothing to do.
+
+		endCallback();
+	}
+}
