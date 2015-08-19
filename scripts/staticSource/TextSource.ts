57738da503643a1115d3bbc7de2a01f2b26b4bb6
Christian Brel <ch.brel@gmail.com>
2015-08-19 12:01:51 +0200

    refs CORECLIENT-83 : Added new static source for Text.

---
+++ scripts/staticSource/TextSource.ts
@@ -0,0 +1,32 @@
+/**
+ * @author Christian Brel <christian@the6thscreen.fr, ch.brel@gmail.com>
+ * @author Simon Urli <simon@the6thscreen.fr, simon.urli@gmail.com>
+ */
+
+/// <reference path="../../t6s-core/core/scripts/infotype/TextList.ts" />
+/// <reference path="../../t6s-core/core/scripts/infotype/Text.ts" />
+/// <reference path="./StaticSource.ts" />
+
+/**
+ * Represent a static Logo Source in the client
+ */
+class TextSource extends StaticSource<TextList> {
+
+	/**
+	 * Create and return the information of the Static Source
+	 *
+	 * @method computeInfo
+	 */
+	computeInfo() : TextList {
+
+		var text : Text = new Text();
+		text.setId(this.params.TextValue);
+		text.setValue(this.params.TextValue);
+
+		var textList : TextList = new TextList();
+		textList.setId(this.params.TextValue);
+		textList.addText(text);
+
+		return textList;
+	}
+}
