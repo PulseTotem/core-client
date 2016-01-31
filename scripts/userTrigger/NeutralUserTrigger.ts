1c4b3c324de8d3a53d46b1564331877707087825
Christian Brel <ch.brel@gmail.com>
2016-01-31 18:37:36 +0100

    refs CORECLIENT-120 : Added a neutral User Trigger.

---
+++ scripts/userTrigger/NeutralUserTrigger.ts
@@ -0,0 +1,24 @@
+/**
+ * @author Christian Brel <christian@pulsetotem.fr, ch.brel@gmail.com>
+ * @author Simon Urli <simon@pulsetotem.fr, simon.urli@gmail.com>
+ */
+
+/// <reference path="./UserTrigger.ts" />
+
+/**
+ * Represents "Neutral" User Trigger of PulseTotem Client.
+ *
+ * @class NeutralUserTrigger
+ * @extends UserTrigger
+ */
+class NeutralUserTrigger extends UserTrigger {
+
+	/**
+	 * Constructor.
+	 *
+	 * @constructor
+	 */
+	constructor() {
+		super();
+	}
+}
