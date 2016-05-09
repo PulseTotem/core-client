50bc408de6403135428e44b215b48ea20f1cdef7
Christian Brel <ch.brel@gmail.com>
2016-05-09 18:18:40 +0200

    refs CORECLIENT-144 : Added new renderer for FeedNode Info Type.

--- scripts/renderer/FeedNode/ForPersonIntroductionFeedNodeRenderer.ts
+++ scripts/renderer/FeedNode/ForPersonIntroductionFeedNodeRenderer.ts
@@ -9,7 +9,7 @@
 declare var $: any; // Use of JQuery
 declare var moment: any; // Use of MomentJS
 
-class TopDownFeedNodeRenderer implements Renderer<FeedNode> {
+class ForPersonIntroductionFeedNodeRenderer implements Renderer<FeedNode> {
 
 	/**
 	 * Transform the Info list to another Info list.
@@ -35,7 +35,7 @@ class TopDownFeedNodeRenderer implements Renderer<FeedNode> {
 			for(var iFN in fcFeedNodes) {
 				var fn : FeedNode = fcFeedNodes[iFN];
 
-				if((fn.getSummary() != null && fn.getSummary().trim() != "") || (fn.getDescription() != null && fn.getDescription().trim() != "") || (fn.getMediaUrl() != null && fn.getMediaUrl() != "")) {
+				if((fn.getTitle() != null && fn.getTitle().trim() != "") || (fn.getSummary() != null && fn.getSummary().trim() != "") || (fn.getDescription() != null && fn.getDescription().trim() != "") || (fn.getMediaUrl() != null && fn.getMediaUrl() != "")) {
 					feedNodes.push(fn);
 
 					if (fn.getMediaUrl() != null && fn.getMediaUrl() != "") {
@@ -60,35 +60,72 @@ class TopDownFeedNodeRenderer implements Renderer<FeedNode> {
 	 */
 	render(info : FeedNode, domElem : any, rendererTheme : string, endCallback : Function) {
 		var nodeHTMLWrapper = $("<div>");
-		nodeHTMLWrapper.addClass("TopDownFeedNodeRenderer_wrapper");
+		nodeHTMLWrapper.addClass("ForPersonIntroductionFeedNodeRenderer_wrapper");
 		nodeHTMLWrapper.addClass(rendererTheme);
 
 		//FeedContent logo
 		var feedcontentHTMLLogoContainer = $("<div>");
-		feedcontentHTMLLogoContainer.addClass("TopDownFeedNodeRenderer_feedcontent_logo_container");
+		feedcontentHTMLLogoContainer.addClass("ForPersonIntroductionFeedNodeRenderer_feedcontent_logo_container");
 
 		nodeHTMLWrapper.append(feedcontentHTMLLogoContainer);
 
 		var feedcontentHTMLLogoShadow = $("<div>");
-		feedcontentHTMLLogoShadow.addClass("TopDownFeedNodeRenderer_feedcontent_logo_shadow");
+		feedcontentHTMLLogoShadow.addClass("ForPersonIntroductionFeedNodeRenderer_feedcontent_logo_shadow");
 
 		feedcontentHTMLLogoContainer.append(feedcontentHTMLLogoShadow);
 
 		var feedcontentHTMLLogoColor = $("<div>");
-		feedcontentHTMLLogoColor.addClass("TopDownFeedNodeRenderer_feedcontent_logo_color");
+		feedcontentHTMLLogoColor.addClass("ForPersonIntroductionFeedNodeRenderer_feedcontent_logo_color");
 
 		feedcontentHTMLLogoContainer.append(feedcontentHTMLLogoColor);
 
 		var feedcontentHTMLLogo = $("<div>");
-		feedcontentHTMLLogo.addClass("TopDownFeedNodeRenderer_feedcontent_logo");
+		feedcontentHTMLLogo.addClass("ForPersonIntroductionFeedNodeRenderer_feedcontent_logo");
 
 		feedcontentHTMLLogoContainer.append(feedcontentHTMLLogo);
 
-		//TitleContainer
-		var titleContainer = $("<div>");
-		titleContainer.addClass("TopDownFeedNodeRenderer_title_container");
+		//Main
+		var nodeMain = $("<div>");
+		nodeMain.addClass("ForPersonIntroductionFeedNodeRenderer_main");
 
-		nodeHTMLWrapper.append(titleContainer);
+		nodeHTMLWrapper.append(nodeMain);
+
+		//Main -> Picture
+		if(info.getMediaUrl() != null && info.getMediaUrl() != "") {
+			var nodeMainPicture = $("<div>");
+			nodeMainPicture.addClass("ForPersonIntroductionFeedNodeRenderer_main_picture");
+			nodeMainPicture.addClass("pull-left");
+
+			nodeMain.append(nodeMainPicture);
+
+			nodeMainPicture.css("background-image", "url('" + info.getMediaUrl() + "')");
+		}
+
+		//Main -> Message
+		var nodeMainMessage = $("<div>");
+		nodeMainMessage.addClass("ForPersonIntroductionFeedNodeRenderer_main_message");
+		nodeMainMessage.addClass("pull-left");
+
+		nodeMain.append(nodeMainMessage);
+
+		if(((info.getTitle() != null && info.getTitle().trim() != "") || (info.getSummary() != null && info.getSummary().trim() != "") || (info.getDescription() != null && info.getDescription().trim() != "")) && info.getMediaUrl() != null && info.getMediaUrl() != "") {
+			nodeMainMessage.addClass("ForPersonIntroductionFeedNodeRenderer_main_message_with_picture");
+		} else {
+			nodeMainMessage.addClass("ForPersonIntroductionFeedNodeRenderer_main_message_empty");
+			nodeMainPicture.addClass("ForPersonIntroductionFeedNodeRenderer_main_picture_alone");
+		}
+
+		//Main -> Message -> TitleContainer
+		var titleContainer = $("<div>");
+		titleContainer.addClass("ForPersonIntroductionFeedNodeRenderer_main_message_title_container");
+
+		nodeMainMessage.append(titleContainer);
+
+		//Main -> Message -> TitleContainer -> Title
+		var titleDiv = $("<div>");
+		titleDiv.addClass("ForPersonIntroductionFeedNodeRenderer_main_message_title_container_title");
+
+		titleContainer.append(titleDiv);
 
 		var titleContainerSpan = $("<span>");
 		if(info.getTitle() != null) {
@@ -96,114 +133,49 @@ class TopDownFeedNodeRenderer implements Renderer<FeedNode> {
 		} else {
 			titleContainerSpan.html("");
 		}
-		titleContainer.append(titleContainerSpan);
+		titleDiv.append(titleContainerSpan);
 
-		//Main
-		var nodeMain = $("<div>");
-		nodeMain.addClass("TopDownFeedNodeRenderer_main");
+		//Main -> Message -> Container
+		var messageContainer = $("<div>");
+		messageContainer.addClass("ForPersonIntroductionFeedNodeRenderer_main_message_container");
 
-		nodeHTMLWrapper.append(nodeMain);
+		nodeMainMessage.append(messageContainer);
 
-		//Main -> Arrow
-		var nodeMainArrow = $("<div>");
-		nodeMainArrow.addClass("TopDownFeedNodeRenderer_main_arrow");
-		nodeMainArrow.html("&#9652;");
-		nodeMain.append(nodeMainArrow);
+		//Main -> Message -> Container -> Arrow
+		var nodeMessageArrow = $("<div>");
+		nodeMessageArrow.addClass("ForPersonIntroductionFeedNodeRenderer_main_message_content_arrow");
+		nodeMessageArrow.html("&#9652;");
+		messageContainer.append(nodeMessageArrow);
 
-		//Main -> Message
-		var nodeMainMessage = $("<div>");
-		nodeMainMessage.addClass("TopDownFeedNodeRenderer_main_message");
-		nodeMainMessage.addClass("pull-left");
+		//Main -> Message -> Container -> Content
+		var messageContent = $("<div>");
+		messageContent.addClass("ForPersonIntroductionFeedNodeRenderer_main_message_content");
 
-		nodeMain.append(nodeMainMessage);
+		messageContainer.append(messageContent);
 
 		var nodeMainMessageSpan = $("<span>");
 		if(info.getDescription() != null) {
 			nodeMainMessageSpan.html(info.getDescription());
+		} else if(info.getSummary() != null) {
+			nodeMainMessageSpan.html(info.getSummary());
 		}
-		nodeMainMessage.append(nodeMainMessageSpan);
-
-		//Main -> Picture
-		if(info.getMediaUrl() != null && info.getMediaUrl() != "") {
-			var nodeMainPicture = $("<div>");
-			nodeMainPicture.addClass("TopDownFeedNodeRenderer_main_picture");
-			nodeMainPicture.addClass("pull-left");
-
-			nodeMain.append(nodeMainPicture);
-
-			nodeMainPicture.css("background-image", "url('" + info.getMediaUrl() + "')");
-
-			if(info.getDescription() != null && info.getDescription().trim() != "") {
-				nodeMainMessage.addClass("TopDownFeedNodeRenderer_main_message_with_picture");
-			} else {
-				nodeMainMessage.addClass("TopDownFeedNodeRenderer_main_message_empty");
-				nodeMainPicture.addClass("TopDownFeedNodeRenderer_main_picture_alone");
-			}
-		}
+		messageContent.append(nodeMainMessageSpan);
 
 		//Clearfix for Main
 		var clearFixMain = $("<div>");
 		clearFixMain.addClass("clearfix");
 		nodeMain.append(clearFixMain);
 
-		//Footer
-		var nodeFooter = $("<div>");
-		nodeFooter.addClass("TopDownFeedNodeRenderer_footer");
-
-		nodeHTMLWrapper.append(nodeFooter);
-
-		//Footer -> Author
-		var nodeFooterAuthor = $("<div>");
-		nodeFooterAuthor.addClass("TopDownFeedNodeRenderer_footer_author");
-		nodeFooterAuthor.addClass("pull-left");
-
-		var nodeFooterAuthorSpan = $("<span>");
-		if(info.getAuthor() != null) {
-			nodeFooterAuthorSpan.html(info.getAuthor());
-		}
-		nodeFooterAuthor.append(nodeFooterAuthorSpan);
-
-		nodeFooter.append(nodeFooterAuthor);
-
-		//Footer -> date
-		var nodeFooterDate = $("<div>");
-		nodeFooterDate.addClass("TopDownFeedNodeRenderer_footer_date");
-		nodeFooterDate.addClass("pull-right");
-
-		nodeFooter.append(nodeFooterDate);
-
-		var nodeFooterDateSpan = $("<span>");
-
-		if(info.getCreationDate() != null) {
-			var creationDate:any = moment(info.getCreationDate());
-			var displayCreationDate = creationDate.fromNow();
-			nodeFooterDateSpan.html(displayCreationDate);
-		}
-
-		nodeFooterDate.append(nodeFooterDateSpan);
-
-		var clearFixNodeFooter = $("<div>");
-		clearFixNodeFooter.addClass("clearfix");
-		nodeFooter.append(clearFixNodeFooter);
-
 		$(domElem).css("overflow", "visible");
 		$(domElem).append(nodeHTMLWrapper);
 
-		titleContainer.textfill({
+		titleDiv.textfill({
 			minFontPixels: 30,
 			maxFontPixels: 600
 		});
 
-		nodeMainMessage.textfill({
+		messageContent.textfill({
 			minFontPixels: 30,
-			maxFontPixels: 600
-		});
-
-		nodeFooterAuthor.textfill({
-			maxFontPixels: 600
-		});
-
-		nodeFooterDate.textfill({
 			maxFontPixels: 600
 		});
 
@@ -222,35 +194,35 @@ class TopDownFeedNodeRenderer implements Renderer<FeedNode> {
 	updateRender(info : FeedNode, domElem : any, rendererTheme : string, endCallback : Function) {
 		if(info.getMediaUrl() != null && info.getMediaUrl() != "") {
 
-			var nodeMainPictures = $(domElem).find(".TopDownFeedNodeRenderer_main_picture");
+			var nodeMainPictures = $(domElem).find(".ForPersonIntroductionFeedNodeRenderer_main_picture");
 			var nodeMainPicture;
 			if(nodeMainPictures.length > 0) {
 				nodeMainPicture = nodeMainPictures.first();
 			} else {
 				nodeMainPicture = $("<div>");
-				nodeMainPicture.addClass("TopDownFeedNodeRenderer_main_picture");
+				nodeMainPicture.addClass("ForPersonIntroductionFeedNodeRenderer_main_picture");
 				nodeMainPicture.addClass("pull-left");
 			}
 
 			nodeMainPicture.css("background-image", "url('" + info.getMediaUrl() + "')");
 
-			var nodeMainMessage = $(domElem).find(".TopDownFeedNodeRenderer_main_message").first();
-			nodeMainMessage.removeClass("TopDownFeedNodeRenderer_main_message_with_picture");
-			nodeMainMessage.removeClass("TopDownFeedNodeRenderer_main_message_empty");
-			nodeMainMessage.removeClass("TopDownFeedNodeRenderer_main_picture_alone");
+			var nodeMainMessage = $(domElem).find(".ForPersonIntroductionFeedNodeRenderer_main_message").first();
+			nodeMainMessage.removeClass("ForPersonIntroductionFeedNodeRenderer_main_message_with_picture");
+			nodeMainMessage.removeClass("ForPersonIntroductionFeedNodeRenderer_main_message_empty");
+			nodeMainMessage.removeClass("ForPersonIntroductionFeedNodeRenderer_main_picture_alone");
 
 			if(info.getDescription() != null && info.getDescription().trim() != "") {
-				nodeMainMessage.addClass("TopDownFeedNodeRenderer_main_message_with_picture");
+				nodeMainMessage.addClass("ForPersonIntroductionFeedNodeRenderer_main_message_with_picture");
 			} else {
-				nodeMainMessage.addClass("TopDownFeedNodeRenderer_main_message_empty");
-				nodeMainPicture.addClass("TopDownFeedNodeRenderer_main_picture_alone");
+				nodeMainMessage.addClass("ForPersonIntroductionFeedNodeRenderer_main_message_empty");
+				nodeMainPicture.addClass("ForPersonIntroductionFeedNodeRenderer_main_picture_alone");
 			}
 
 		} else {
-			$(domElem).find(".TopDownFeedNodeRenderer_main_picture").remove();
+			$(domElem).find(".ForPersonIntroductionFeedNodeRenderer_main_picture").remove();
 		}
 
-		var nodeFooterAuthor = $(domElem).find(".TopDownFeedNodeRenderer_footer_author").first();
+		var nodeFooterAuthor = $(domElem).find(".ForPersonIntroductionFeedNodeRenderer_footer_author").first();
 		nodeFooterAuthor.empty();
 		var nodeFooterAuthorSpan = $("<span>");
 		if(info.getAuthor() != null) {
@@ -262,7 +234,7 @@ class TopDownFeedNodeRenderer implements Renderer<FeedNode> {
 			maxFontPixels: 600
 		});
 
-		var nodeFooterDate = $(domElem).find(".TopDownFeedNodeRenderer_footer_date").first();
+		var nodeFooterDate = $(domElem).find(".ForPersonIntroductionFeedNodeRenderer_footer_date").first();
 		nodeFooterDate.empty();
 
 		var nodeFooterDateSpan = $("<span>");
@@ -280,7 +252,7 @@ class TopDownFeedNodeRenderer implements Renderer<FeedNode> {
 		});
 
 
-		var nodeMainMessage = $(domElem).find(".TopDownFeedNodeRenderer_main_message").first();
+		var nodeMainMessage = $(domElem).find(".ForPersonIntroductionFeedNodeRenderer_main_message").first();
 		nodeMainMessage.empty();
 
 		var nodeMainMessageSpan = $("<span>");
@@ -295,7 +267,7 @@ class TopDownFeedNodeRenderer implements Renderer<FeedNode> {
 			maxFontPixels: 600
 		});
 
-		var titleContainer = $(domElem).find(".TopDownFeedNodeRenderer_title_container").first();
+		var titleContainer = $(domElem).find(".ForPersonIntroductionFeedNodeRenderer_title_container").first();
 		titleContainer.empty();
 
 		var titleContainerSpan = $("<span>");
