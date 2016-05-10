06b2d6eb5427d55d1b6f84d088c46b18a2a9f956
Simon Urli <simon.urli@gmail.com>
2016-05-10 10:52:47 +0200

    CORECLIENT-145 A first simple renderer for mosaic picture

--- scripts/renderer/Cmd/MosaicPictureRenderer.ts
+++ scripts/renderer/Cmd/MosaicPictureRenderer.ts
@@ -50,10 +50,12 @@ class MosaicPictureRenderer implements Renderer<Cmd> {
     render(info : Cmd, domElem : any, rendererTheme : string, endCallback : Function) {
 
         if (info.getCmd() == "counterMosaic") {
+            $(domElem).empty();
             var nbPics = parseInt(info.getArgs()[0]);
             var totalPics = parseInt(info.getArgs()[1]);
+            var messageText = info.getArgs()[2];
 
-            var percent = (nbPics / totalPics) * 100;
+            var percent = Math.round((nbPics / totalPics) * 100);
 
             var progressBarContainer = $('<div>');
             progressBarContainer.addClass('progress');
@@ -64,9 +66,9 @@ class MosaicPictureRenderer implements Renderer<Cmd> {
             progressBar.addClass('progress-bar-striped');
             progressBar.addClass('active');
 
-            if (percent < 50) {
+            if (percent < 70) {
                 progressBar.addClass('progress-bar-danger');
-            } else if (percent < 75) {
+            } else if (percent < 85) {
                 progressBar.addClass('progress-bar-warning');
             } else {
                 progressBar.addClass('progress-bar-success');
@@ -76,27 +78,76 @@ class MosaicPictureRenderer implements Renderer<Cmd> {
             progressBar.attr('aria-valuenow', percent);
             progressBar.attr('aria-valuemin',0);
             progressBar.attr('aria-valuemax',100);
-            progressBar.css('height','60%');
+            progressBar.css('height',percent+'%');
             progressBar.text(percent+'%');
 
             progressBarContainer.append(progressBar);
 
             domElem.append(progressBarContainer);
 
-        } else if  (info.getCmd() == "startProcessing") {
+            var divMessage = $('<div>');
+            divMessage.addClass("MosaicPictureRenderer_message");
 
-            var infoProcessing = $('<div>');
-            infoProcessing.addClass('jumbotron');
-            infoProcessing.text('Picture start to process...');
-            domElem.append(infoProcessing);
+            var divMessageContent = $('<div>');
+            divMessageContent.addClass("MosaicPictureRenderer_message_content");
+
+            var messageSpan = $('<span>');
+            messageSpan.text(messageText);
+            divMessageContent.append(messageSpan);
+            divMessage.append(divMessageContent);
+            $(domElem).append(divMessage);
+
+            divMessage.textfill({
+                maxFontPixels: 500
+            });
+
+        } else if  (info.getCmd() == "startProcessing") {
+            $(domElem).empty();
+            var progressBarContainer = $('<div>');
+            progressBarContainer.addClass('progress');
+            progressBarContainer.addClass('progress-bar-vertical');
+
+            var progressBar = $('<div>');
+            progressBar.addClass('progress-bar');
+            progressBar.addClass('progress-bar-striped');
+            progressBar.addClass('active');
+            progressBar.addClass('progress-bar-success');
+
+            progressBar.attr('role', 'progress-bar');
+            progressBar.attr('aria-valuenow', percent);
+            progressBar.attr('aria-valuemin',0);
+            progressBar.attr('aria-valuemax',100);
+            progressBar.css('height','100%');
+            progressBar.text('100%');
+
+            progressBarContainer.append(progressBar);
+
+            domElem.append(progressBarContainer);
+
+            var divMessage = $('<div>');
+            divMessage.addClass("MosaicPictureRenderer_message");
+
+            var divMessageContent = $('<div>');
+            divMessageContent.addClass("MosaicPictureRenderer_message_content");
+
+            var messageSpan = $('<span>');
+            messageSpan.text("La mosaïque est en cours de génération...");
+            divMessageContent.append(messageSpan);
+            divMessage.append(divMessageContent);
+            $(domElem).append(divMessage);
+
+            divMessage.textfill({
+                maxFontPixels: 500
+            });
+            domElem.append(divMessage);
 
         } else if (info.getCmd() == "mosaicProcessed") {
+            $(domElem).empty();
             var urlPic = info.getArgs()[0];
 
-            var pic = $('<img>');
-            pic.addClass('img-responsive');
-            pic.addClass('img-rounded');
-            pic.attr('src',urlPic);
+            var pic = $('<div>');
+            pic.addClass('MosaicPictureRenderer_pic');
+            pic.css("background-image", "url('" + urlPic + "')")
             domElem.append(pic);
         }
     }
