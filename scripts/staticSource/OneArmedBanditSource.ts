332cbf41332ab6225eafca56708b90fddef9bb52
Christian Brel <ch.brel@gmail.com>
2016-05-25 10:18:00 +0200

    refs CORECLIENT-166 : First version for OneArmedBandit static source.

--- scripts/staticSource/OneArmedBanditSource.ts
+++ scripts/staticSource/OneArmedBanditSource.ts
@@ -4,17 +4,36 @@
 
 /// <reference path="./StaticSource.ts" />
 /// <reference path="../../t6s-core/core/scripts/infotype/CmdList.ts" />
+/// <reference path="../../t6s-core/core/scripts/infotype/Cmd.ts" />
 /// <reference path="../core/MessageBus.ts" />
 /// <reference path="../core/MessageBusChannel.ts" />
 /// <reference path="../core/MessageBusAction.ts" />
 
+declare var uuid: any; // Use of uuid
+
 /**
  * Represents the "OneArmedBandit" StaticSource of PulseTotem Client.
  *
- * @class OneArmedBandit
+ * @class OneArmedBanditSource
  * @extends StaticSource<CmdList>
  */
-class OneArmedBandit extends StaticSource<CmdList> {
+class OneArmedBanditSource extends StaticSource<CmdList> {
+
+	/**
+	 * OneArmedBandit's counter.
+	 *
+	 * @property _counter
+	 * @type number
+	 */
+	private _counter : number;
+
+	/**
+	 * OneArmedBandit's hash.
+	 *
+	 * @property _oneArmedBanditHash
+	 * @type string
+	 */
+	private _oneArmedBanditHash : string;
 
 	/**
 	 * Constructor.
@@ -25,6 +44,9 @@ class OneArmedBandit extends StaticSource<CmdList> {
 	 */
 	constructor(refreshTime : number = 60, params : any = []) {
 		super(refreshTime, params);
+
+		this._counter = null;
+		this._oneArmedBanditHash = uuid.v1();
 	}
 
 	/**
@@ -39,11 +61,9 @@ class OneArmedBandit extends StaticSource<CmdList> {
 			self.getCall().onNewInfo(info);
 		}
 
-		//TODO
-
-		MessageBus.subscribe(MessageBusChannel.RENDERER, function(channel : any, data : any) {
-			if(typeof(data.action) != "undefined" && data.action == MessageBusAction.SELECT) {
-				var info = self.computeSelectionFromRenderer(data.message);
+		MessageBus.subscribe(MessageBusChannel.USERTRIGGER, function(channel : any, data : any) {
+			if(typeof(data.action) != "undefined" && data.action == MessageBusAction.TRIGGER) {
+				var info = self.computeInfo();
 
 				if (info != null) {
 					self.getCall().onNewInfo(info);
@@ -58,18 +78,36 @@ class OneArmedBandit extends StaticSource<CmdList> {
 	 * @method computeInfo
 	 */
 	computeInfo() : CmdList {
-		// SlaveStaticSource doesn't need this method.
-		return null;
-	}
+		if(this._counter != null) {
+			if(this._counter > 0) {
+				this._counter = this._counter - 1;
+			}
+		} else {
+			if(typeof(this.params.InitialValue) != "undefined" && parseInt(this.params.InitialValue) >= 0) {
+				this._counter = parseInt(this.params.InitialValue);
+			} else {
+				this._counter = 50;
+			}
+		}
 
-	/**
-	 * Create and return the information of the Static Source
-	 *
-	 * @method computeIncomingMessageFromTimeline
-	 * @param {any} selectionMessage - The selection message.
-	 */
-	computeSelectionFromRenderer(selectionMessage : any) : CmdList {
-		Logger.debug("SlaveStaticSource - computeSelectionFromRenderer - Method need to be implemented !");
-		return null;
+		var cmd : Cmd = new Cmd();
+		cmd.setId(this._oneArmedBanditHash);
+		cmd.setDurationToDisplay(this.params.InfoDuration);
+		cmd.setCmd("Stop");
+
+		var args : Array<string> = new Array<string>();
+		if(this._counter > 0) {
+			args.push("true");
+		} else {
+			args.push("true");
+		}
+		cmd.setArgs(args);
+
+		var cmdList : CmdList = new CmdList();
+		cmdList.setId(this._oneArmedBanditHash);
+		cmdList.addCmd(cmd);
+		cmdList.setDurationToDisplay(this.params.InfoDuration);
+
+		return cmdList;
 	}
 }
